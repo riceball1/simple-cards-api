b6680433a00fe9371ee9168e7c38b77898f96dd0
Dana Ng <ngdana1@gmail.com>
2023-03-16 01:51:52 -0700

    Fix type error

--- src/pages/api/user/[useremail].ts
+++ src/pages/api/user/[useremail].ts
@@ -1,17 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import userData from "@/pages/database/userData.json";
+import { UserData } from "@/helper/types";
 
-type Data = {
-  userID: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  age: number;
-};
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<UserData>
 ) {
   const useremail = req.query.useremail;
   const user = userData.filter((user) => {
