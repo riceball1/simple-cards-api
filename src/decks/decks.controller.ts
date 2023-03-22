4428c016ad25eef2f0a4193f5a51c229578a87aa
Dana Ng <ngdana1@gmail.com>
2023-03-21 23:41:57 -0700

    Update crud for decks

--- src/decks/decks.controller.ts
+++ src/decks/decks.controller.ts
@@ -1,33 +1,55 @@
-import { Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateDeckDto } from './dto/create-deck.dto';
+import { UpdateDeckDto } from './dto/update-deck.dto';
+import { DecksService } from './decks.service';
 
 @Controller('decks')
 export class DecksController {
-    // GET /decks --> []
-    @Get()
-    getDecks(@Query('type') type: string) {
-        return [{type}]
-    }
 
-    // GET /decks/:id --> {...}
-    @Get(':id')
-    getOneDeck(@Param('id') id: string) {
-        return {id}
-    }
-    // POST /decks
-    @Post()
-    createDeck() {
-        return {}
-    }
+    /*
+    Note: due to the DecksService has @Injectable by providing the param here to the constructor, nestjs will instanitate the DecksService
 
-    // PUT /decks/:id --> {...}
-    @Put(':id')
-    updateDeck(@Param('id') id: string) {
-        return {id}
-    }
+    Should not need to manually instanitate classes in nestjs, most will automatically happen
 
-    // DELETE /decks/:id --> {...}
-    @Delete(':id')
-    deleteDeck(@Param('id') id: string) {
-        return {id}
-    }
+    */
+    constructor(private readonly decksService: DecksService) {}
+
+
+  // GET /decks --> []
+  @Get()
+  getDecks(@Query('name') name: string) {
+    return this.decksService.getDecks(name);
+  }
+
+  // GET /decks/:id --> {...}
+  @Get(':id')
+  getOneDeck(@Param('id') id: string) {
+    return this.decksService.getDeck(+id)
+  }
+  // POST /decks
+  @Post()
+  createDeck(@Body() createDeckDto: CreateDeckDto) {
+    return this.decksService.createDeck(createDeckDto)
+  }
+
+  // PUT /decks/:id --> {...}
+  @Put(':id')
+  updateDeck(@Param('id') id: string, @Body() updateDeckDto: UpdateDeckDto) {
+    return this.decksService.updateDeck(+id, updateDeckDto)
+  }
+
+  // DELETE /decks/:id --> {...}
+  @Delete(':id')
+  deleteDeck(@Param('id') id: string) {
+    return this.decksService.removeDeck(+id)
+  }
 }
--- src/decks/decks.service.ts
+++ src/decks/decks.service.ts
@@ -1,4 +1,56 @@
+import { CreateDeckDto } from './dto/create-deck.dto';
+import { UpdateDeckDto } from './dto/update-deck.dto';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class DecksService {}
+export class DecksService {
+  private decks = [
+    { id: 0, name: 'spanish' },
+    { id: 1, name: 'russian' },
+  ];
+
+  getDecks(name?: string) {
+    if (name) {
+      return this.decks.filter((deck) => deck.name === name)[0];
+    }
+    return this.decks;
+  }
+
+  getDeck(id: number) {
+    const deck = this.decks.find((deck) => deck.id === id);
+
+    if (!deck) {
+      throw new Error('deck not found');
+    }
+    return deck;
+  }
+
+  createDeck(createDeckDto: CreateDeckDto) {
+    const newDeck = {
+      ...createDeckDto,
+      id: Date.now(),
+    };
+
+    this.decks.push(newDeck);
+
+    return newDeck;
+  }
+
+  updateDeck(id: number, updateDeckDto: UpdateDeckDto) {
+    this.decks = this.decks.map((deck) => {
+      if (deck.id === id) {
+        return { ...deck, ...updateDeckDto };
+      }
+      return deck;
+    });
+
+    return this.getDeck(id);
+  }
+
+  removeDeck(id: number) {
+    const toBeRemoved = this.getDeck(id);
+    this.decks = this.decks.filter((deck) => deck.id !== id);
+
+    return toBeRemoved;
+  }
+}
